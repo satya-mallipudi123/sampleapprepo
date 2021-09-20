38eef60982da7e2ea69da85399098ead55dea23c
satya-mallipudi123 <msatya@LIN65003913.corp.capgemini.com>
2021-09-20 11:55:57 +0530

    sum added

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -9,6 +9,12 @@ public class BootClass {
 
 	}
 	public static void show() {
+		int[] arr= {1,2,3,4,4};
+		int sum=0;
+		for(int n:arr) {
+			sum+=n;
+			System.out.println(n);
+		}
 		System.out.println("show method here");
 	}
 
